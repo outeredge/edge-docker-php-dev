79d25d9800b80db7f0fda86f606e70f54eea169a
David Windell <david@outeredgeuk.com>
2019-12-12 01:08:10 +0000

    Add PS1

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -9,4 +9,6 @@ if [ ${CURRENT_GID} -ne 1000 ]; then
   fi
 fi
 
+echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" > /home/edge/.bashrc
+
 exec "$@"
