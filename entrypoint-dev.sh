b7f5b47da0b85b1b887b2ec353c2f41d1da8bda4
David Windell <david@outeredgeuk.com>
2019-12-17 23:58:22 +0000

    Simplify PS1

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -9,6 +9,6 @@ if ! whoami &> /dev/null; then
   fi
 fi
 
-echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" > /home/edge/.bashrc
+echo "PS1='\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" > /home/edge/.bashrc
 
 exec "$@"
