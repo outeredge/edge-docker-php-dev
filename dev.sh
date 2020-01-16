ffe361759fb72bd4155f4c6fe1606f2206e46d44
David Windell <david@outeredgeuk.com>
2020-01-16 17:42:40 +0000

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if [ ! -d "/home/edge/.composer" ] || [ -n "$(ls -A "/home/edge/.composer")" ]; then
+if [ ! -d "/home/edge/.composer/vendor" ] || [ -n "$(ls "/home/edge/.composer/vendor")" ]; then
   cp -rT /home/edge/.composer.orig /home/edge/.composer &
 fi
 
