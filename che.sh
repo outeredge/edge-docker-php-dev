558eecc752dbe1595f87a032d92d1f970d657839
David Windell <david@outeredgeuk.com>
2020-02-04 15:38:31 +0000

    Add timeout and prevent hard fail

--- che.sh
+++ che.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-CHE_INFO=$(curl -s $CHE_API/workspace/$CHE_WORKSPACE_ID?token=$CHE_MACHINE_TOKEN)
-export RUNTIME_URL=$(echo $CHE_INFO | jq -re '.runtime.machines[] | select(.attributes.source == "recipe").servers | first(..|.url? | select(.!=null))')
+CHE_INFO=$(curl --silent --max-time 5 $CHE_API/workspace/$CHE_WORKSPACE_ID?token=$CHE_MACHINE_TOKEN)
+export RUNTIME_URL=$(echo $CHE_INFO | jq -re '.runtime.machines[]? | select(.attributes.source == "recipe").servers | first(..|.url? | select(.!=null))')
+
