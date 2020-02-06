8ad8fb81f5c8c08548783b753139526d21871938
David Windell <david@outeredgeuk.com>
2020-02-06 15:53:21 +0000

    Move runtime url detection to boot, don't hard fail with -e

--- .bashrc
+++ .bashrc
@@ -1,7 +1,7 @@
-source /etc/profile.d/che.sh
 source /etc/profile.d/bash_completion.sh
 source /etc/profile.d/n98-magerun.phar.bash
 export PS1='\[\033[01;32m\]\[\033[0m\033[0;32m\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '
 export VISUAL=nano
 export EDITOR="$VISUAL"
+export RUNTIME_URL=$(cat /tmp/runtime.url)
 [ ! -z "$WEB_ROOT" ] && cd $WEB_ROOT
--- dev.sh
+++ dev.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
+
+CHE_INFO=$(curl --silent --max-time 5 $CHE_API/workspace/$CHE_WORKSPACE_ID?token=$CHE_MACHINE_TOKEN)
+RUNTIME_URL=$(echo $CHE_INFO | jq -re '.runtime.machines[]? | select(.attributes.source == "recipe").servers | first(..|.url? | select(.!=null))')
+echo "$RUNTIME_URL" > "/tmp/runtime.url"
 
 if [ ! -d "/home/edge/.composer/vendor" ] || [ -n "$(ls "/home/edge/.composer/vendor")" ]; then
   cp -rT /home/edge/.composer.orig /home/edge/.composer &
