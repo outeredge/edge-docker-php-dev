39b9987ebd37ec05882533f671564439ea582718
David Windell <david@outeredgeuk.com>
2021-03-03 16:51:33 +0000

    Improve .bashrc, add caches for composer, yarn and npm on gitpod

--- gitpod.sh
+++ gitpod.sh
@@ -3,3 +3,6 @@
 export NGINX_PORT=8080
 export RUNTIME_URL=$(gp url $NGINX_PORT)
 export WEB_ROOT=$GITPOD_REPO_ROOT
+export COMPOSER_HOME=/workspace/.composer
+export YARN_CACHE_FOLDER=/workspace/.yarn
+export npm_config_cache=/workspace/.npm
