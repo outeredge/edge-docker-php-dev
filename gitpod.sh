d210c71c7442f4c5c0b368410249856577b3d1f5
David Windell <david@outeredgeuk.com>
2021-03-11 17:56:34 +0000

    Add trailing slash to runtime URL

--- gitpod.sh
+++ gitpod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RUNTIME_URL=$(gp url $NGINX_PORT)
+export RUNTIME_URL=$(gp url $NGINX_PORT)/
 export WEB_ROOT=$GITPOD_REPO_ROOT
 export COMPOSER_HOME=/workspace/.composer
 export YARN_CACHE_FOLDER=/workspace/.yarn
