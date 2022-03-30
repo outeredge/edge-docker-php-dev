9cb70b9b4a9edcc3162a8a7a26e54d2777ee1507
David Windell <david@outeredgeuk.com>
2022-03-30 14:36:39 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "No version specified, exiting!"
+    exit
+fi;
+
+echo "Building image outeredge/edge-docker-php-dev:$1 with Dockerfile.php${1//./}"
+docker build --pull . -t outeredge/edge-docker-php-dev:$1 -f Dockerfile.php${1//./}
+docker push outeredge/edge-docker-php-dev:$1
+echo "Complete!"
