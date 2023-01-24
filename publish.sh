a5cbe6a671685e3c2f8d96c9283f338e2e437a35
David Windell <david@outeredgeuk.com>
2023-01-24 18:08:02 +0000

    Add release version support to publish script

--- publish.sh
+++ publish.sh
@@ -5,8 +5,20 @@ if [ -z "$1" ]; then
     exit
 fi;
 
+r=""
+if [ ! -z "$2" ]; then
+    r="-$2"
+fi;
+
 if [ ! -z "$DOCKER_PASS" ]; then
     echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+fi;
+
+if [ ! -z "$r" ]; then
+    echo "Building image outeredge/edge-docker-php-dev:$1$r with Dockerfile.php${1//./}"
+    DOCKER_BUILDKIT=1 docker build --pull . -t outeredge/edge-docker-php-dev:$1$r -f Dockerfile.php${1//./} && \
+    docker push outeredge/edge-docker-php-dev:$1$r && \
+    echo "Complete!"
 fi;
 
 echo "Building image outeredge/edge-docker-php-dev:$1 with Dockerfile.php${1//./}"
