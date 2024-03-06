3127810b6491fe57ef9fc6529c17ae7e2c33ca10
David Windell <david@outeredgeuk.com>
2024-03-06 16:54:55 +0000

    Allow overriding WEB_ROOT with GITPOD_REPO_ROOT in .env

--- gitpod.sh
+++ gitpod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RUNTIME_URL=$(gp url $NGINX_PORT)/
-export WEB_ROOT=$GITPOD_REPO_ROOT
+export WEB_ROOT=${WEB_ROOT_GITPOD:-$GITPOD_REPO_ROOT}
 export COMPOSER_HOME=/workspace/.composer
 export YARN_CACHE_FOLDER=/workspace/.yarn
 export npm_config_cache=/workspace/.npm
