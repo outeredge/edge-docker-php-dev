34e772a7ee44332c9b164588f41422abcdd0a3d5
David Windell <david@outeredgeuk.com>
2025-09-09 11:01:27 +0000

    Ignore hidden paths in /workspaces

--- ona.sh
+++ ona.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RUNTIME_URL=$(gitpod environment port open 8080 --name nginx)/
-export WEB_ROOT=$(find /workspaces/ -mindepth 1 -maxdepth 1 -type d | head -n 1)
+export WEB_ROOT=$(find /workspaces/ -maxdepth 1 -mindepth 1 -type d -not -name ".*" | head -n 1)
 export COMPOSER_HOME=/workspaces/.composer
 export YARN_CACHE_FOLDER=/workspaces/.yarn
 export npm_config_cache=/workspaces/.npm
