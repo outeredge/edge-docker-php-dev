96ff0d9b63c2cfddf0530dba068f17e84c07c7dc
David Windell <david@outeredgeuk.com>
2025-09-11 17:06:49 +0000

    Change target path for caches

--- dev.sh
+++ dev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export COMPOSER_HOME="/home/$(whoami)/.composer"
+export YARN_CACHE_FOLDER="/home/$(whoami)/.yarn"
+export npm_config_cache="/home/$(whoami)/.npm"
 
 if [ ! -z "${GITPOD_ENVIRONMENT_ID}" ]; then
     # We are running in Ona
--- ona.sh
+++ ona.sh
@@ -2,6 +2,3 @@
 
 export RUNTIME_URL=$(gitpod environment port open 8080 --name nginx)/
 export WEB_ROOT=$(find /workspaces/ -maxdepth 1 -mindepth 1 -type d -not -name ".*" | head -n 1)
-export COMPOSER_HOME=/workspaces/.composer
-export YARN_CACHE_FOLDER=/workspaces/.yarn
-export npm_config_cache=/workspaces/.npm
