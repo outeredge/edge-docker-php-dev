8dccff3a44bd5c079ce9ae04a52170423aa3f075
David Windell <david@outeredgeuk.com>
2026-05-27 16:16:28 +0100

    Update meldit command for preview URL generation

--- workspace.sh
+++ workspace.sh
@@ -8,7 +8,7 @@ else
     CACHE_READY=false
 
     if command -v meldit > /dev/null 2>&1; then
-        if RAW_ME=$(meldit me) && RAW_PREVIEW=$(meldit preview 8080 --raw); then
+        if RAW_ME=$(meldit me) && RAW_PREVIEW=$(meldit port 8080 --public --raw); then
             WEB_ROOT=$(echo "$RAW_ME" | jq -r '.workspacePath')
             RUNTIME_URL="$RAW_PREVIEW/"
             CACHE_READY=true
